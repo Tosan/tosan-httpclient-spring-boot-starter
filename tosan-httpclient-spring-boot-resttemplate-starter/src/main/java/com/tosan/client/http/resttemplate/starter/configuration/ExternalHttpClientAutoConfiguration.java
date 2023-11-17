3e7fafd7e4ee81af4194fbe25903abde20fe0eb3
mosidev <abdollahi.dev@gmail.com>
2023-11-17 19:42:58 +0330

    [enhancement] update tosan-mask dependency version

--- tosan-httpclient-spring-boot-resttemplate-starter/src/main/java/com/tosan/client/http/resttemplate/starter/configuration/ExternalHttpClientAutoConfiguration.java
+++ tosan-httpclient-spring-boot-resttemplate-starter/src/main/java/com/tosan/client/http/resttemplate/starter/configuration/ExternalHttpClientAutoConfiguration.java
@@ -4,6 +4,7 @@ import com.tosan.client.http.core.Constants;
 import com.tosan.client.http.resttemplate.starter.util.HttpLoggingInterceptorUtil;
 import com.tosan.tools.mask.starter.config.SecureParameter;
 import com.tosan.tools.mask.starter.config.SecureParametersConfig;
+import com.tosan.tools.mask.starter.configuration.MaskBeanConfiguration;
 import com.tosan.tools.mask.starter.replace.JacksonReplaceHelper;
 import com.tosan.tools.mask.starter.replace.JsonReplaceHelperDecider;
 import com.tosan.tools.mask.starter.replace.RegexReplaceHelper;
@@ -14,32 +15,29 @@ import org.springframework.context.annotation.Configuration;
 
 import java.util.HashSet;
 
-import static com.tosan.tools.mask.starter.configuration.MaskBeanConfiguration.SECURED_PARAMETERS;
-
 @Configuration
 public class ExternalHttpClientAutoConfiguration {
 
     @Bean({"http-client-util-regex-replace-helper"})
     @ConditionalOnMissingBean(name = "http-client-util-regex-replace-helper")
-    public JsonReplaceHelperDecider replaceHelperDecider(JacksonReplaceHelper jacksonReplaceHelper,
-                                                         RegexReplaceHelper regexReplaceHelper,
-                                                         @Qualifier("http-client-util-secured-parameters")
-                                                         SecureParametersConfig secureParametersConfig) {
+    public JsonReplaceHelperDecider replaceHelperDecider(
+            JacksonReplaceHelper jacksonReplaceHelper, RegexReplaceHelper regexReplaceHelper,
+            @Qualifier("http-client-util-secured-parameters") SecureParametersConfig secureParametersConfig) {
         return new JsonReplaceHelperDecider(jacksonReplaceHelper, regexReplaceHelper, secureParametersConfig);
     }
 
     @Bean({"http-client-util-secured-parameters"})
     @ConditionalOnMissingBean(name = "http-client-util-secured-parameters")
     public SecureParametersConfig secureParametersConfig() {
-        HashSet<SecureParameter> securedParameters = new HashSet<>(SECURED_PARAMETERS);
+        HashSet<SecureParameter> securedParameters = new HashSet<>(MaskBeanConfiguration.SECURED_PARAMETERS);
         securedParameters.add(Constants.AUTHORIZATION_SECURE_PARAM);
         securedParameters.add(Constants.PROXY_AUTHORIZATION_SECURE_PARAM);
         return new SecureParametersConfig(securedParameters);
     }
 
     @Bean
-    public HttpLoggingInterceptorUtil httpLoggingInterceptorUtil(@Qualifier("http-client-util-regex-replace-helper")
-                                                                 JsonReplaceHelperDecider replaceHelperDecider) {
+    public HttpLoggingInterceptorUtil httpLoggingInterceptorUtil(
+            @Qualifier("http-client-util-regex-replace-helper") JsonReplaceHelperDecider replaceHelperDecider) {
         return new HttpLoggingInterceptorUtil(replaceHelperDecider);
     }
 }
